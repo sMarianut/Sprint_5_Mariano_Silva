430a950ddf4a2629e1111b915284aa2bfe49fcac
sMarianut <smarianut@gmail.com>
2023-07-21 00:23:39 +0200

    VUe aplicado home, up and past.

---
+++ assets/scripts/past.js
@@ -0,0 +1,41 @@
+const { createApp } = Vue
+const options = {
+    data() {
+        return {
+
+            eventos: [],
+            categories: [],
+            filter: [],
+            inputValue: "",
+            checkbox: [],
+            inputCheck: [],
+            crossFilter: []
+
+        }
+    },
+    created() {
+        fetch('https://mindhub-xj03.onrender.com/api/amazing')
+            .then(response => response.json())
+            .then(datosEventos => {
+                this.eventos = datosEventos.events.filter(evento => evento.date < datosEventos.currentDate)
+                this.crossFilter = this.eventos
+                this.categories = [... new Set(this.eventos.map(events => events.category))]
+                console.log(this.categories);
+                //console.log(this.eventos);
+
+            })
+            .catch(error => console.error(error))
+    },
+    methods: {
+        bothFilter() {
+            this.crossFilter = this.eventos.filter(evento => {
+                return evento.name.toLowerCase().startsWith(this.inputValue.toLowerCase())
+                    && (this.checkbox.includes(evento.category) || this.checkbox.length == 0)
+            })
+        }
+    }
+
+
+}
+const app = createApp(options)
+app.mount('#app')
